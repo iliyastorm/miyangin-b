50f1bf1b0880ec84bb872deb86c05dfd84066928
iliyastorm <theiliyastorm@gmail.com>
2023-02-06 22:04:33 +0330

    کد فورم
    نوشتن کد فورم برای ارزسال اعداد

--- index.php
+++ index.php
@@ -8,9 +8,17 @@ if (isset($_POST['submit'])) {
     if ($n3 > $max) $max = $n3;
     echo $max;
     echo "<br>";
-    echo $b=$n1+$n2+$n3;
+    echo $b = $n1 + $n2 + $n3;
     echo "<br>";
-    echo $b/3;
+    echo $b / 3;
 }
 ?>
-
+<form method="post">
+    <label> number1:</label><br/>
+    <input type="number" name="n1"/></br>
+    <label>number2:</label></br>
+    <input type="number" name="n2"/></br>
+    <label>number3:</label></br>
+    <input type="number" name="n3"/></br>
+    <input type="submit" name="submit" value="run"/>
+</form>
